d31d6ac3db829923687d2eef96c047a4554bc0a6
Alena <alenapikulina01@gmail.com>
2025-07-03 18:35:43 +0300

    task three ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,6 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) return false;
+    return arr1.every((el, index) => el === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
